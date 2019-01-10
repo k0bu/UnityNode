39030ad74e4e692ba27d050eef5f267784a3fd5d
k0bu <30616294+k0bu@users.noreply.github.com>
2019-01-10 14:14:14 +0900

    Imported socket io asset in Unity

---
+++ MultiplayerNode/Assets/Scripts/Networking/NetworkClient.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+//Shortcut command C-k,C-d
+
+namespace Project.Networking {
+    //Mark the namespace "SocketIOComponent" and press F12 to jump to the definition
+    //and modify some properties
+    public class NetworkClient : SocketIOComponent {
+
+
+    }
+}
+
