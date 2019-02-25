0194976c034e08c1a69e39e0cbda9012c66f656d
k0bu <kazuki.tada@keio.jp>
2019-02-25 11:38:35 +0900

    babel-eslint -> eslint:recommended

--- NodejsServer/.eslintrc.js
+++ NodejsServer/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    "extends": "eslint:recommended",//parser: 'babel-eslint',
     ecmaVersion: 6
   },
   extends: [
